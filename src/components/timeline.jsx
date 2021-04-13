b61390d5c0c2e237c5e318454ff0c5939fc896ed
rishav1802 <rishabh.kumar1802@gmail.com>
2021-04-13 14:06:50 +0530

    first commit

---
+++ src/components/timeline.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Integrated Post Graduation at IIIT-Gwl<span>2016-2021</span></h2>
+                        <p>I am currently in my final year of the IPG Course and during my course projects I have worked with machine learning, OpenSource projects and have undertaken courses like Oops, Data Strucures, IOT, Computer Networks Artificial Intelligence, DataBase Management System, System Analysis and Design and have good grasp over these subjects. </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2003-2015</span></h2>
+                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths and CS in c++ as extra with 91 percentage. During my time at school, I have developed interest in solving complex problems of mathematics including Permutation and Combination, Aptitude and Reasoning. <br />  I completed my Primary Education from an ICSE School, St. Thomas Ranchi with 94.2 percentage.  </p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
